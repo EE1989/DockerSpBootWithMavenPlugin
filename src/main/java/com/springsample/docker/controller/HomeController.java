bd046fcb5429d056ab8575ea80cd2bd0c0d88864
Emre <emre.eker@ymail.com>
2017-12-04 12:06:11 +0300

    first commit

---
+++ src/main/java/com/springsample/docker/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.springsample.docker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Spring Boot from Docker";
+	}
+	
+	
+}
